cfc5602bafa5c147111b24848a4bd590859c541b
Rashmi <rashmiarane1411@gmail.com>
2023-08-31 18:56:49 +0530

    Survay Form

---
+++ survayform.php
@@ -0,0 +1,43 @@
+<!-- <?
+//variable setting
+
+// $firstname=$_REQUEST['firstname'];
+// $lastname=$_REQUEST['lastname'];
+// $email=$_REQUEST['email'];
+// $mobilenumber=$_REQUEST['mobilenumber'];
+// $address=$_REQUEST['address'];
+// $paddress=$_REQUEST['paddress'];
+// $country=$_REQUEST['country'];
+// $age=$_REQUEST['age"'];
+// $gender=$_REQUEST['gender'];
+// $status=$_REQUEST['status'];
+// $hobby=$_REQUEST['hobby'];
+// $dbirth=$_REQUEST['dbirth'];
+// $dcompleted=$_REQUEST['dcompleted'];
+// $clgname=$_REQUEST['clgname'];
+// $pc=$_REQUEST['pc'];
+// $dpursuing=$_REQUEST['dpursuing'];
+// $collegename=$_REQUEST['collegename'];
+// $percgpa=$_REQUEST['percgpa'];
+// $company=$_REQUEST['company'];
+// $interest=$_REQUEST['interest'];
+// $lf=$_REQUEST['lf'];
+// $city=$_REQUEST['city'];
+// $languages=$_REQUEST['languages'];
+// $comment=$_REQUEST['comment'];
+
+// //check input fields
+// if(empty($firstname)||empty($lastname)||empty($email)||empty($mobilenumber)||empty($address)||empty($paddress)||
+// empty($country)||empty($age)||empty($status)||empty($hobby)||empty($dbirth)||empty($dcompleted)||empty($clgname)
+// ||empty($pc)||empty($dpursuing)||empty($collegename)||empty($percgpa)||empty($company)||empty($interest)||empty($lf)
+// ||empty($city)||empty($languages)||empty($comment)){
+//     echo "please fill the field";
+// }
+// else{
+//     mail("rashmiarane1411@gmail.com", "Survay Form",  $message ,"From: $firstname $lastname<$email>" );
+//     echo "<script type='text/javascript'>alert('Your form submitted succesfully in gmail');
+//     window.history.log(-1);
+// </script>";
+// }
+
+?> -->
